d8d69a7d669989047a26c8379ec6b12e4af1f24a
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2020-11-03 19:53:27 +0200

    Update index.js

--- regexp/index.js
+++ regexp/index.js
@@ -1,14 +1,24 @@
 document.querySelector('#user-form').addEventListener('submit', function (e) {
-	e.preventDefault();
+    e.preventDefault();
+    var fullName = document.querySelector('[name="full_name"]');
+    var email = document.querySelector('[name="email"]');
+    var password = document.querySelector('[name="password"]');
 
+    var regexpOfFullName = /^[а-яА-ЯїЇіІєЄ]+\s[а-яА-ЯїЇіІєЄ]+\s[а-яА-ЯїЇіІєЄ]+$/;
+    var regexpOfEmail = /^[a-zA-Z0-9-][a-zA-Z.0-9-]+[a-zA-Z0-9-]@[a-zA-Z0-9-][a-zA-Z.0-9-]+[.][a-zA-Z.0-9-]+[a-zA-Z0-9-]$/;
+    var regexpOfPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$/;
+
+    fullName.style.backgroundColor = regexpOfFullName.test(fullName.value)?'#c2e0c6':'#f9d0c4';
+    email.style.backgroundColor = regexpOfEmail.test(email.value)?'#c2e0c6':'#f9d0c4';
+    password.style.backgroundColor = regexpOfPassword.test(password.value)?'#c2e0c6':'#f9d0c4';
 
 });
 
 document.querySelectorAll('[data-show]').forEach(function (button) {
-	button.addEventListener('click', function (e) {
-		document.querySelector('#description').classList.add('d-none');
-		document.querySelector('#preview').classList.add('d-none');
+    button.addEventListener('click', function (e) {
+        document.querySelector('#description').classList.add('d-none');
+        document.querySelector('#preview').classList.add('d-none');
 
-		document.querySelector('#' + e.currentTarget.getAttribute('data-show')).classList.remove('d-none');
-	});
+        document.querySelector('#' + e.currentTarget.getAttribute('data-show')).classList.remove('d-none');
+    });
 });
