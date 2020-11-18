8f4ba38f35647f2680258cbf02034aad97fc6f7e
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2020-11-18 22:17:40 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -7,27 +7,27 @@ function Csv() {
             let tab = [...string];
 
             const commaLength = comma.reduce((pr, item, index) =>{
-                if(!(item.indexOf(',')===-1)){comma[index]=item.split(',');}
-                if(!(pr===undefined)){if(!(pr===comma[index].length)){return -1;}}
-                if (pr===-1){return;}
+                if(!(item.indexOf(',')===-1))comma[index]=item.split(',');
+                if(!(pr===undefined))if(!(pr===comma[index].length))return -1;
+                if (pr===-1)return;
                 return comma[index].length;
             }, undefined)
             const semicolonLength = semicolon.reduce((pr, item, index) =>{
-                if(!(item.indexOf(';')===-1)){semicolon[index]=item.split(';');}
-                if(!(pr===undefined)){if(!(pr===semicolon[index].length)){return -1;}}
-                if (pr===-1){return;}
+                if(!(item.indexOf(';')===-1))semicolon[index]=item.split(';');
+                if(!(pr===undefined))if(!(pr===semicolon[index].length))return -1;
+                if (pr===-1)return;
                 return semicolon[index].length;
             }, undefined)
             const tabLength = tab.reduce((pr, item, index) =>{
-                if(!(item.indexOf(' ')===-1)){tab[index]=item.split(' ');}
-                if(!(pr===undefined)){if(!(pr===tab[index].length)){return -1;}}
-                if (pr===-1){return;}
+                if(!(item.indexOf(' ')===-1))tab[index]=item.split(' ');
+                if(!(pr===undefined))if(!(pr===tab[index].length))return -1;
+                if (pr===-1)return;
                 return tab[index].length;
             }, undefined)
 
-            if(!(commaLength===-1)){string=[...comma];}
-            if(!(semicolonLength===-1)){string=[...semicolon];}
-            if(!(tabLength===-1)){string=[...tab];}
+            if(!(commaLength===-1))string=[...comma];
+            if(!(semicolonLength===-1))string=[...semicolon];
+            if(!(tabLength===-1))string=[...tab];
 
             return string;
         }
@@ -35,9 +35,7 @@ function Csv() {
         return string;
     }
     this.generate = (array, separator) =>{
-        if (separator===undefined){
-            return array.join(',');
-        }
+        if (separator===undefined) return array.join(',');
         return array.join(separator);
     }
 }
