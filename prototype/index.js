c6560680576c94f307b26baf0e747fc41a17fd3c
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2020-11-19 12:07:18 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -1,41 +1,42 @@
 function Csv() {
-    this.parse = (string, separator) =>{
-        string = string.split('\n');
-        if (separator===undefined){
-            let comma = [...string];
-            let semicolon = [...string];
-            let tab = [...string];
 
-            const commaLength = comma.reduce((pr, item, index) =>{
-                if(!(item.indexOf(',')===-1))comma[index]=item.split(',');
-                if(!(pr===undefined))if(!(pr===comma[index].length))return -1;
-                if (pr===-1)return;
-                return comma[index].length;
-            }, undefined)
-            const semicolonLength = semicolon.reduce((pr, item, index) =>{
-                if(!(item.indexOf(';')===-1))semicolon[index]=item.split(';');
-                if(!(pr===undefined))if(!(pr===semicolon[index].length))return -1;
-                if (pr===-1)return;
-                return semicolon[index].length;
-            }, undefined)
-            const tabLength = tab.reduce((pr, item, index) =>{
-                if(!(item.indexOf(' ')===-1))tab[index]=item.split(' ');
-                if(!(pr===undefined))if(!(pr===tab[index].length))return -1;
-                if (pr===-1)return;
-                return tab[index].length;
-            }, undefined)
+}
+Csv.prototype.parse = (string, separator) =>{
+    string = string.split('\n');
+    if (separator===undefined){
+        let comma = [...string];
+        let semicolon = [...string];
+        let tab = [...string];
 
-            if(!(commaLength===-1))string=[...comma];
-            if(!(semicolonLength===-1))string=[...semicolon];
-            if(!(tabLength===-1))string=[...tab];
+        const commaLength = comma.reduce((pr, item, index) =>{
+            if(!(item.indexOf(',')===-1))comma[index]=item.split(',');
+            if(!(pr===undefined))if(!(pr===comma[index].length))return -1;
+            if (pr===-1)return;
+            return comma[index].length;
+        }, undefined)
+        const semicolonLength = semicolon.reduce((pr, item, index) =>{
+            if(!(item.indexOf(';')===-1))semicolon[index]=item.split(';');
+            if(!(pr===undefined))if(!(pr===semicolon[index].length))return -1;
+            if (pr===-1)return;
+            return semicolon[index].length;
+        }, undefined)
+        const tabLength = tab.reduce((pr, item, index) =>{
+            if(!(item.indexOf(' ')===-1))tab[index]=item.split(' ');
+            if(!(pr===undefined))if(!(pr===tab[index].length))return -1;
+            if (pr===-1)return;
+            return tab[index].length;
+        }, undefined)
 
-            return string;
-        }
-        string.map((item, index)=>string[index]=item.split(separator));
+        if(!(commaLength===-1))string=[...comma];
+        if(!(semicolonLength===-1))string=[...semicolon];
+        if(!(tabLength===-1))string=[...tab];
+
         return string;
     }
-    this.generate = (array, separator) =>{
-        if (separator===undefined) return array.join(',');
-        return array.join(separator);
-    }
+    string.map((item, index)=>string[index]=item.split(separator));
+    return string;
+}
+Csv.prototype.generate = (array, separator) =>{
+    if (separator===undefined) return array.join(',');
+    return array.join(separator);
 }
