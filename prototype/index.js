0cb2184a76e816133adbb4e308e2f5ccc9caeb02
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2020-11-19 20:22:13 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -1,7 +1,9 @@
+const alphabet_a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numeral = ['1', '2', '3','4', '5', '6', '7', '8', '9', '10', '11','12', '13', '14', '15', '16','17','18', '19', '20', '21', '22', '23', '24', '25', '26'];
 function Csv() {
 
 }
-Csv.prototype.parse = (string, separator) => {
+const parseString = function parseStr(string, separator) {
   let stringCsv = string.split('\n');
   if (separator === undefined) {
     const comma = [...stringCsv];
@@ -10,20 +12,23 @@ Csv.prototype.parse = (string, separator) => {
 
     const commaLength = comma.reduce((pr, item, index) => {
       if (!(item.indexOf(',') === -1)) comma[index] = item.split(',');
+      else return -1;
       if (!(pr === undefined)) if (!(pr === comma[index].length)) return -1;
-      if (pr === -1) return;
+      if (pr === -1) return -1;
       return comma[index].length;
     }, undefined);
     const semicolonLength = semicolon.reduce((pr, item, index) => {
       if (!(item.indexOf(';') === -1)) semicolon[index] = item.split(';');
+      else return -1;
       if (!(pr === undefined)) if (!(pr === semicolon[index].length)) return -1;
-      if (pr === -1) return;
+      if (pr === -1) return -1;
       return semicolon[index].length;
     }, undefined);
     const tabLength = tab.reduce((pr, item, index) => {
       if (!(item.indexOf(' ') === -1)) tab[index] = item.split(' ');
+      else return -1;
       if (!(pr === undefined)) if (!(pr===tab[index].length)) return -1;
-      if (pr === -1) return;
+      if (pr === -1) return -1;
       return tab[index].length;
     }, undefined);
 
@@ -36,7 +41,11 @@ Csv.prototype.parse = (string, separator) => {
   stringCsv = stringCsv.map((item, index) => stringCsv[index] = item.split(separator));
   return stringCsv;
 };
-Csv.prototype.generate = (array, separator) => {
+Csv.prototype.parse = (string, separator) => {
+  if (separator === undefined) return parseString(string);
+  return  parseString(string, separator);
+}
+Csv.prototype.generate = function generateString(array, separator) {
   let arrayCsv = [...array];
   let joinArray = function joinArr(localSeparator) {
     if (!(array[0][0].length === 1)) {
@@ -48,7 +57,17 @@ Csv.prototype.generate = (array, separator) => {
   if (separator === undefined) return joinArray(',', arrayCsv);
   return joinArray(separator, arrayCsv);
 };
+
+
 function CsvArray() {
 
 }
-CsvArray = new Array(0);
+CsvArray.prototype = Object.create(Array.prototype);
+CsvArray.prototype.parse = (string, separator) => {
+  let csvArray;
+  if (separator === undefined) csvArray = parseString(string);
+  else csvArray = parseString(string, separator);
+  csvArray = csvArray.map((item) => CsvArray.prototype.push(item));
+  return csvArray;
+};
+//CsvArray.prototype.generate = () => {};
