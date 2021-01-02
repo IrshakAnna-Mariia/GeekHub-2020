3ccb692deb0feb328754b9e75e098ca44cb83ff3
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-01-02 15:15:12 +0200

    Update DoubleSlider.js

--- react-slider/src/DoubleSlider.js
+++ react-slider/src/DoubleSlider.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const widthBar = 360;
-
 export default class DoubleSlider extends React.PureComponent {
     constructor(props) {
         super(props);
@@ -11,7 +9,8 @@ export default class DoubleSlider extends React.PureComponent {
             max: this.props.max,
             startValue: this.props.startValue,
             endValue: this.props.endValue,
-            onChange: this.props.onChange
+            onChange: this.props.onChange,
+            widthBar: 360
         }
 
         this.onChangeStartInput = (e) => {
@@ -32,7 +31,7 @@ export default class DoubleSlider extends React.PureComponent {
         document.body.addEventListener('mouseup', this.onDragEnd1);
     };
     onDrag1 = (e) => {
-        let {min, max} = this.state;
+        let {min, max, widthBar} = this.state;
         let newValue = Math.round((((e.clientX - this.firstX)*(max-min)/widthBar)+this.valueX));
         if ((newValue >= min) && (newValue <= max)) {this.setState({startValue: newValue});}
         else if ((newValue < min)) {this.setState({startValue: min});}
@@ -40,6 +39,7 @@ export default class DoubleSlider extends React.PureComponent {
     };
     onDragEnd1 = (e) => {
         this.onDrag1(e);
+        this.onChange();
         document.body.removeEventListener('mousemove', this.onDrag1);
         document.body.removeEventListener('mouseup', this.onDragEnd1);
     };
@@ -52,7 +52,7 @@ export default class DoubleSlider extends React.PureComponent {
         document.body.addEventListener('mouseup', this.onDragEnd2);
     };
     onDrag2 = (e) => {
-        let {min, max} = this.state;
+        let {min, max, widthBar} = this.state;
         let newValue = Math.round((((e.clientX - this.firstX)*(max-min)/widthBar)+this.valueX));
         if ((newValue >= min) && (newValue <= max)) {this.setState({endValue: newValue});}
         else if ((newValue < min)) {this.setState({endValue: min});}
@@ -60,35 +60,45 @@ export default class DoubleSlider extends React.PureComponent {
     };
     onDragEnd2 = (e) => {
         this.onDrag2(e);
+        this.onChange();
         document.body.removeEventListener('mousemove', this.onDrag2);
         document.body.removeEventListener('mouseup', this.onDragEnd2);
     };
 
-    valueOnSlider(min, max, value) {
+    valueOnSlider(min, max, value, widthBar) {
         if (value < min) {return 0}
         else if (value > max) {return widthBar}
         return (widthBar*((value-min)/(max-min)));
     }
 
+    onChange = () => {
+        let {startValue, endValue} = this.state;
+        if (startValue > endValue) {
+            let minChange = endValue;
+            this.setState({endValue: startValue})
+            this.setState({startValue: minChange})
+        }
+    }
+
     render() {
-        let {min, max, startValue, endValue} = this.state;
+        let {min, max, startValue, endValue, widthBar} = this.state;
         return (
             <Root>
                 <input value={startValue} onChange={this.onChangeStartInput}/>
                 <input value={endValue} onChange={this.onChangeEndInput}/>
 
-                <Bar>
+                <Bar width={widthBar}>
                     <Bar2
                         width={widthBar * (endValue - min) / (max - min)-widthBar * (startValue - min) / (max - min)}
                         marginLeft={widthBar * (startValue - min) / (max - min)}
                     >
                         <Handler
-                            value={this.valueOnSlider(min, max, startValue)}
+                            value={this.valueOnSlider(min, max, startValue, widthBar)}
                             onMouseDown={this.onDragStart1}
                             marginLeft={(widthBar * (startValue - min) / (max - min))*(-1)}
                         />
                         <Handler
-                            value={this.valueOnSlider(min, max, endValue)}
+                            value={this.valueOnSlider(min, max, endValue, widthBar)}
                             onMouseDown={this.onDragStart2}
                             marginLeft={(widthBar * (startValue - min) / (max - min))*(-1)}
                         />
@@ -109,7 +119,7 @@ const Root = styled.div`
 
 const Bar = styled.div`
     position: relative;
-    width: ${widthBar + 'px'};
+    width: ${p => p.width + 'px'};
 	height: 2px;
 	background-color: black;
     margin-top: 10px;
