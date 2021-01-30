d7f86d4c09cdc99930849031a9a569df2f443b39
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-01-30 22:15:25 +0200

    Update index.js

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {createStore} from "redux"
+import { createStore} from "redux"
 import {Provider} from 'react-redux'
 import '../css/base.css'
 import '../css/index.css'
 import './store/base.js'
-import todoListApp from "./store/reducers"
+import todoListApp, {} from "./store/reducers"
 import App from './store/app'
-import {addTodo, isCompleted, removeTodo, setFilter, Filters, setCounter} from "./store/actions";
+import {addTodo, isCompleted, removeTodo, viewTodos, viewAll, viewActive, viewCompleted, counterCompletedTodo} from "./store/actions";
 
 const store = createStore(todoListApp);
-const {ALL, ACTIVE, COMPLETED} = Filters;
 
 render();
 store.subscribe(render);
@@ -22,13 +21,19 @@ function render () {
             <App
                 onAdd={(e) => {
                     store.dispatch(addTodo(e.target.value));
+                    store.dispatch(viewTodos(store.getState().todoList));
+                    store.dispatch(counterCompletedTodo(store.getState().todoList));
                 }}
-                todoList={store.getState().todoList}
+                todoList={store.getState().rootReducers.toolkit.currentTodos}
                 onRemove={(e) => {
                     store.dispatch(removeTodo(Number(e.target.value)));
+                    store.dispatch(viewTodos(store.getState().todoList));
+                    store.dispatch(counterCompletedTodo(store.getState().todoList));
                 }}
                 onCompleted={(e) => {
                     store.dispatch(isCompleted(Number(e.target.value)));
+                    store.dispatch(viewTodos(store.getState().todoList));
+                    store.dispatch(counterCompletedTodo(store.getState().todoList));
                 }}
                 onClearCompleted={() => {
                     let arrayCompleted = store.getState().todoList.map((cell, index) => {
@@ -41,7 +46,19 @@ function render () {
                             store.dispatch(removeTodo(Number(cell)));
                         }
                     });
+                    store.dispatch(viewTodos(store.getState().todoList));
+                    store.dispatch(counterCompletedTodo(store.getState().todoList));
                 }}
+                onAll = {() => {
+                    store.dispatch(viewAll(store.getState().todoList));
+                }}
+                onActive = {() => {
+                    store.dispatch(viewActive(store.getState().todoList));
+                }}
+                onCompletedClick = {() => {
+                    store.dispatch(viewCompleted(store.getState().todoList));
+                }}
+                count = {store.getState().rootReducers.toolkit.counter}
             />
         </Provider>,
         document.getElementById('root')
