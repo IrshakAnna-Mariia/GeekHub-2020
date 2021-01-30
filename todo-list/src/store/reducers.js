d2a0619deabd2324ee7cfbb9289af7da7c8b9712
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-01-30 22:16:14 +0200

    Update reducers.js

--- todo-list/src/store/reducers.js
+++ todo-list/src/store/reducers.js
@@ -1,16 +1,61 @@
-import {ADD_TODO, REMOVE_TODO, SET_FILTER, Filters, IS_COMPLETED, SET_COUNTER} from "./actions";
+import {ADD_TODO, REMOVE_TODO, IS_COMPLETED} from "./actions";
 import {combineReducers} from "redux";
+import {createSlice} from '@reduxjs/toolkit'
 
-const {ALL} = Filters;
+const initialState = [{text: "Taste JavaScript", complete: true}, {text: "Buy a unicorn", complete: false}];
 
-function visualFilter(state = ALL, action) {
-    switch (action.type) {
-        case SET_FILTER:
-            return action.filter;
-        default:
-            return state;
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: {
+        counter: 1,
+        currentTodos: initialState
+    },
+    reducers: {
+        viewTodos(state, action) {
+            state.currentTodos = [...action.payload];
+        },
+        all: (state, action) => {
+            state.currentTodos = [...action.payload];
+        },
+        active: (state, action) => {
+            let arrayActiveFinally = [];
+            let arrayActive = action.payload.map((cell) => {
+                if (cell.complete === false) {
+                    return cell
+                }
+            })
+            arrayActive.map((cell) => {
+                if (!(cell === undefined)) {
+                    arrayActiveFinally.push(cell);
+                }
+            });
+            state.currentTodos = [...arrayActiveFinally];
+        },
+        completed: (state, action) => {
+            let arrayCompleteFinally = [];
+            let arrayComplete = action.payload.map((cell) => {
+                if (cell.complete === true) {
+                    return cell
+                }
+            })
+            arrayComplete.map((cell) => {
+                if (!(cell === undefined)) {
+                    arrayCompleteFinally.push(cell);
+                }
+            });
+            state.currentTodos = [...arrayCompleteFinally];
+        },
+        count: (state, action) => {
+            let count = 0;
+            action.payload.map((cell) => {
+                if (cell.complete === false) {
+                    count = count + 1;
+                }
+            })
+            state.counter = count;
+        },
     }
-}
+});
 
 function todoList(state = [
     {text: "Taste JavaScript", complete: true},
@@ -41,9 +86,14 @@ function todoList(state = [
     }
 }
 
+const rootReducers = combineReducers({
+    toolkit: filterSlice.reducer
+})
+
 const todoListApp = combineReducers({
-    visualFilter,
+    rootReducers,
     todoList
 });
 
 export  default todoListApp;
+export const {addTodos, isCompleted, removeTodo, completed, active, all, count} = filterSlice.actions
