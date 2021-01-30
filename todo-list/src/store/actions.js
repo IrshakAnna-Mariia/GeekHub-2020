4563f323da4f9e7b0ddae42e67d1daa7a31cd21d
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-01-30 22:18:14 +0200

    Update actions.js

--- todo-list/src/store/actions.js
+++ todo-list/src/store/actions.js
@@ -1,14 +1,6 @@
 export const ADD_TODO = 'ADD_TODO';
 export const REMOVE_TODO = 'REMOVE_TODO';
-export const SET_FILTER = 'SET_FILTER';
 export const IS_COMPLETED = 'IS_COMPLETED';
-export const SET_COUNTER = 'SET_COUNTER';
-
-export const Filters = {
-    ALL: 'ALL',
-    ACTIVE: 'ACTIVE',
-    COMPLETED: 'COMPLETED'
-}
 
 export function addTodo(text) {
     return {type: ADD_TODO, text}
@@ -22,6 +14,22 @@ export function isCompleted(index) {
     return {type: IS_COMPLETED, index}
 }
 
-export function setFilter(filter) {
-    return {type: SET_FILTER, filter}
+export function viewTodos(payload) {
+    return {type: "filter/viewTodos" , payload}
+}
+
+export function viewAll(payload) {
+    return {type: "filter/all" , payload }
+}
+
+export function viewActive(payload) {
+    return {type: "filter/active" , payload }
+}
+
+export function viewCompleted(payload) {
+    return {type: "filter/completed" , payload }
+}
+
+export function counterCompletedTodo(payload) {
+    return {type: "filter/count" , payload }
 }
