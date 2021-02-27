08cafca50ee2c70abb88b9cd7377648adb80661a
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-02-27 23:21:55 +0200

    Add files via upload

---
+++ express/todo-list/src/components/Header.js
@@ -0,0 +1,53 @@
+import React, { Component} from 'react'
+import {connect} from 'react-redux'
+import {addTodo, countAddTodo, removeWrittenTodo, setTodos} from '../store/actions'
+import socket from '../store/socket'
+
+let input;
+
+export default class Header extends Component {
+
+    onChange = (e) => {
+        if (e.key === 'Backspace') {
+            let newValue = input.value.slice(0, input.value.length-1);
+            socket.emit('write_todo', [
+                ...this.props.todos,
+                {text: input.value + e.key, complete: false}
+            ],newValue, input.value)
+        } else {
+            socket.emit('write_todo', [
+                ...this.props.todos,
+                {text: input.value + e.key, complete: false}
+            ], input.value + e.key, input.value)
+        }
+
+        if (e.key === 'Enter'){
+            //this.props.dispatch(removeWrittenTodo(input.value))
+            this.props.dispatch(addTodo(input.value));
+            this.props.dispatch(countAddTodo());
+            this.props.dispatch(setTodos([
+                ...this.props.todos,
+                {text: input.value, complete: false}
+            ]))
+            input.value = "";
+        }
+    }
+    inputHTML = (node) => input = node
+
+    render() {
+        return (
+            <header className="header">
+                <h1>todos</h1>
+                <input
+                    className="new-todo"
+                    placeholder="What needs to be done?"
+                    autoFocus
+                    ref={this.inputHTML}
+                    onKeyDown={this.onChange}
+                />
+            </header>
+        )
+    }
+}
+
+Header = connect()(Header);
