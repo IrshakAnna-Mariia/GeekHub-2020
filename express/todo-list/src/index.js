5b88d5da289fb1b60347d0776587113549088176
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-02-27 23:23:20 +0200

    Add files via upload

---
+++ express/todo-list/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore,applyMiddleware} from "redux"
+import {Provider} from 'react-redux'
+import '../css/base.css'
+import '../css/index.css'
+import './base.js'
+import todoListApp, {} from "./store/reducers"
+import App from './app'
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import {watchSaga} from "./store/sagas";
+import {setTodosForAll, setTodos} from "./store/actions";
+import socket from "./store/socket";
+
+const saga = createSagaMiddleware();
+
+export const store = createStore(todoListApp,applyMiddleware(thunk ,saga));
+saga.run(watchSaga);
+
+render();
+store.subscribe(render);
+store.dispatch(setTodosForAll([]));
+store.dispatch(setTodos([]));
+
+function render () {
+
+    ReactDom.render(
+        <Provider store={store}>
+            <App
+                todoList={store.getState().todoList}
+                todoListActive = {store.getState().rootReducers.toolkit.activeTodos}
+                todoListCompleted = {store.getState().rootReducers.toolkit.completedTodos}
+                todoListCurrent = {store.getState().rootReducers.toolkit.currentTodo}
+                count = {store.getState().rootReducers.toolkit.counter}
+            />
+        </Provider>,
+        document.getElementById('root')
+    )
+}
