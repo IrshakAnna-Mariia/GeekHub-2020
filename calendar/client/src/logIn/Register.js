b48fab03dcd294bfd1bd8ac9fbe6327920bdf598
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-03-31 23:31:03 +0300

    Update Register.js

--- calendar/client/src/logIn/Register.js
+++ calendar/client/src/logIn/Register.js
@@ -1,182 +1,163 @@
-import React, { Component} from 'react'
-import ReactDom from 'react-dom'
-import {connect} from 'react-redux'
-import {Link} from "react-router-dom";
-import {select} from "redux-saga/effects";
+const mongoose = require('mongoose')
+const express = require('express')
+const {resolve} = require('path')
+const fs = require("fs")
+const bodyParser = require("body-parser")
+const bcrypt = require('bcrypt');
+const PORT = process.env.PORT || 8000;
+const User = require('./models/User')
 
-let email;
-let password;
-let repeatPassword;
+mongoose.connect('mongodb://localhost/users', {useUnifiedTopology: true, useNewUrlParser: true})
+    .then(() => console.log('Mongodb has started'))
+    .catch((e) => console.log(e))
+const server = express();
+server.use(express.urlencoded({ extended: false}))
+const httpServer = require('http').createServer(server);
 
-export default class Register extends Component {
+mongoose.connection.on('open', function (){
+    console.log('is cinnected...')
+});
+mongoose.set("useFindAndModify", false);
 
-    onSubmit = (e) => {
+server.use(express.static(
+    resolve(__dirname, "client", "dist")
+));
 
-        if (checkEmail(email.value)
-            && checkPassword(password.value)
-                && (repeatPassword.value===password.value)) {
-            postResponse().then(value => {
-                if(!value) {
-                    ReactDom.render(
-                        <h1 className="resultRegister">
-                            User already exists. Go to the SignIn form
-                        </h1>,
-                        document.getElementById('result')
-                    )
-                }
-            });
-            ReactDom.render(
-                <h1 className="resultRegister">
-                    Registration was successful. Go to the SignIn form
-                </h1>,
-                document.getElementById('result')
-            )
-        } else {
-            if (!checkEmail(email.value)) {
-                ReactDom.render(
-                    <h1 className="incorrectEnteredData">Incorrect entered email</h1>,
-                    document.getElementById('incorrectEmail')
-                )
+server.use(bodyParser.json());
+
+let usersBase = [];
+
+User.find({}, function(err, doc) {
+    if (err) return console.log(err)
+    usersBase = [...doc]
+    console.log(usersBase.map(value => {
+        return value.events
+    }))
+})
+
+server.get("/get", async(req, res) => {
+    try {
+        let usersBase = [];
+
+        User.find({}, function(err, doc) {
+            if (err) return console.log(err)
+            usersBase = [...doc]
+            console.log(usersBase)
+        })
+
+        res.send(usersBase);
+    } catch (e) {
+        console.log(e);
+        res.status(404).jsonp(false)
+    }
+})
+
+server.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        let email = false;
+
+        await User.findOne({email: req.body.email}, (err, doc) => {
+            if (doc) {
+                email = true;
             }
-            if (!checkPassword(password.value)) {
-                ReactDom.render(
-                    <h1 className="incorrectEnteredData">Incorrect entered password. <br/>
-                        Please make sure that entered password have at least 8 characters,
-                        including numbers, lowercase, and uppercase letters</h1>,
-                    document.getElementById('incorrectPassword')
-                )
+        })
+        if (!email) {
+            const newUser = new User({
+                email: req.body.email,
+                password: hashedPassword,
+                events: []
+            })
+            await User.create(newUser, (err) => {
+                if (err) return email = true
+            })
+        }
+        res.status(201).jsonp(email)
+    } catch (e) {
+        res.status(404).jsonp(e)
+    }
+})
+
+server.post("/signIn", async (req, res) => {
+    try {
+        let check = {
+            count: 0,
+            email: false,
+            password: false,
+            events: []
+        }
+        let document = {};
+        await User.findOne({email: req.body.email}, (err, doc) => {
+            if (doc) {
+                check.email = true;
+                check.count = 1;
+                document = {password: doc.password, events: [...doc.events]}
             }
-            if (!(repeatPassword.value === password.value)) {
-                ReactDom.render(
-                    <h1 className="incorrectEnteredData">Incorrect entered password. <br/>
-                        Please make the entered passwords are the same</h1>,
-                    document.getElementById('incorrectRepeatPassword')
-                )
+        })
+        bcrypt.compare(req.body.password, document.password, (error, resp) => {
+            if (resp) {
+                check.events = [...document.events];
+                check.password = true
+                res.status(201).jsonp(check)
             }
+        })
+        if (check.count === 0) {
+            res.status(201).jsonp(check)
         }
 
-        email.value = '';
-        password.value = '';
-        repeatPassword.value = '';
-        e.preventDefault()
-    }
-
-    inputEmail = (node) => email = node
-    inputPassword = (node) => password = node
-    inputRepeatPassword = (node) => repeatPassword = node
-
-    render() {
-        return (
-            <div className="containerReg">
-                <form onSubmit={this.onSubmit}>
-                    <div>
-                        <h1 className="register">Register</h1>
-                        <p className="register">Please fill in this form to create an account</p>
-                        <div id="result">
-                        </div>
-                        <hr/>
-
-                        <label className="email"><b>Email</b></label>
-                        <div id="incorrectEmail">
-                        </div>
-                        <input
-                            type="text"
-                            placeholder="Enter Email"
-                            id="email"
-                            name="email"
-                            ref={this.inputEmail}
-                            required
-                        />
-                        <label className="email"><b>Password</b></label>
-                        <div id="incorrectPassword">
-                        </div>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            id="password"
-                            name="password"
-                            ref={this.inputPassword}
-                            required
-                        />
-                        <label className="email"><b>Repeat password</b></label>
-                        <div id="incorrectRepeatPassword">
-                        </div>
-                        <input
-                            type="password"
-                            placeholder="Enter Password Again"
-                            name="psw-repeat"
-                            ref={this.inputRepeatPassword}
-                            required/>
-
-                        <hr/>
-                    </div>
-                    <Link to='/login'>
-                        <button type="button" className="loginbtn">Sign in</button>
-                    </Link>
-                    <button type="submit"
-                            className="registerbth"
-                    >Register
-                    </button>
-
-                </form>
-            </div>
-        )
-    }
-}
-
-//SignIn = connect()(SignIn);
-
-function checkEmail(propsEmail) {
-    const emailParts = propsEmail.split('@');
-    const username = emailParts[0];
-    const domain = emailParts[1];
-    const usernameRule = /^[a-zA-Z0-9\-.]+$/;
-    const emailRule1 = /^[^.].+[^.]$/;
-    const emailRule2 = /^.*[^.]@[^.].*$/;
-
-    return (
-        emailParts.length === 2 &&
-        usernameRule.test(username) &&
-        usernameRule.test(domain) &&
-        domain.includes('.') &&
-        emailRule1.test(propsEmail) &&
-        emailRule2.test(propsEmail)
-    );
-}
-
-function checkPassword(propsPassword) {
-    const passwordRule1 = /^[a-zA-Z0-9]{8,}$/;
-    const passwordRule2 = /[a-z]/;
-    const passwordRule3 = /[A-Z]/;
-    const passwordRule4 = /[0-9]/;
-
-    return (
-        passwordRule1.test(propsPassword) &&
-        passwordRule2.test(propsPassword) &&
-        passwordRule3.test(propsPassword) &&
-        passwordRule4.test(propsPassword)
-    );
-}
-
-async function postResponse() {
-    try {
-        await fetch("/register", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            },
-            body: JSON.stringify(
-                {email: email.value, password: password.value}
-            ),
-        });
     } catch (e) {
-        console.log(e)
-        ReactDom.render(
-            <h1 className="resultRegister">
-                Something is not work, please reload the page<br/>
-                Error: {e.message}
-            </h1>,
-            document.getElementById('result')
-        )
+        res.status(404).jsonp(false)
+    }
+})
+
+server.post("/add", async (req, res) => {
+    try {
+        let status = true;
+        await User.updateOne({email: req.body.email}, {events: req.body.events}, (err) => {
+            if (err) status = false
+        })
+        res.status(201).jsonp(status)
+    } catch (e) {
+        res.status(404).jsonp(false)
+    }
+})
+
+server.post("/remove", async (req, res) => {
+    try {
+        let returnValue = true;
+
+        await User.findOneAndUpdate({email: req.body.email}, {events: req.body.events}, {new: true},
+            (err) => {
+            if (err) returnValue = false;
+        })
+
+        res.status(201).jsonp(returnValue)
+    } catch (e) {
+        res.status(404).jsonp(false)
+    }
+})
+
+server.post("/edit", async (req, res) => {
+    try {
+        let returnValue = true;
+        await User.findOneAndUpdate({email: req.body.email}, {events: req.body.events}, {new: true},
+            (err) => {
+            if (err) {returnValue = false}
+        })
+
+        res.status(201).jsonp(returnValue)
+    } catch (e) {
+        res.status(404).jsonp(false)
+    }
+});
+
+async function start() {
+    try{
+        httpServer.listen(PORT, () => console.log(`Do it! Port: ${PORT}`))
+    } catch (err) {
+        console.log('Error', err.message)
+        process.exit(1)
     }
 }
+
+start()
