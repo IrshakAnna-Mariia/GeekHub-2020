70057a6c011009d6cdfdf2c90d72653ff36d1781
IrshakAnna-Mariia <73129725+IrshakAnna-Mariia@users.noreply.github.com>
2021-04-02 17:14:00 +0300

    Update actions.js

--- calendar/client/src/store/actions.js
+++ calendar/client/src/store/actions.js
@@ -1,21 +1,23 @@
-export const SET_EVENTS = 'SET_EVENTS';
-
 export function setEvents(payload) {
-    return {type: "filter/setStartEvents", payload}
+    return {type: "event/setStartEvents", payload}
 }
 
 export function addEvent(payload) {
-    return {type: "filter/addNewEvent", payload}
+    return {type: "event/addNewEvent", payload}
 }
 
 export function removeEvent(payload) {
-    return {type: "filter/forRemoveEvent", payload}
+    return {type: "event/forRemoveEvent", payload}
 }
 
 export function editEvent(payload) {
-    return {type: "filter/forEditEvent", payload}
+    return {type: "event/forEditEvent", payload}
 }
 
 export function setEdit(payload) {
-    return {type: "filter/forSetEdit", payload}
+    return {type: "event/forSetEdit", payload}
+}
+
+export function setError(payload) {
+    return {type: "event/forSetError", payload}
 }
